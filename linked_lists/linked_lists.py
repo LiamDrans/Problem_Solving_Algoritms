1a1320740ea3d6b4ef575fcd888aba2baa1220ac
LazySusanoo <liam.drans@gmail.com>
2024-09-16 16:58:17 +0100

    implements linked list add functionality - tested up to three nodes

--- linked_lists/linked_lists.py
+++ linked_lists/linked_lists.py
@@ -1,16 +1,32 @@
 class LinkedList:
     def __init__(self):
-        self._head = None
-        self._tail = None
+        self.head = None
+        self.tail = None
+        self.length = 0 
+
+    def __len__(self):
+        return self.length
 
     def add(self, data):
-        if not self._head:
-            self._head = Node(data)
-        
 
+        if self.length == 0:
+            self.head = Node(data)
+            self.tail = self.head
+            self.length += 1
+        
+        elif self.length == 1:
+            self.head.next = Node(data)
+            self.tail = self.head.next
+            self.length += 1
+        
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+        print(data)
+        
 class Node:
-    def __init__(self, element):
-        self.element = element
-        self._next = None
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 
--- test/test_linked_lists.py
+++ test/test_linked_lists.py
@@ -1,14 +1,31 @@
 from linked_lists.linked_lists import Node, LinkedList
 
-def test_create_Node_from_element():
+def test_create_Node_from_data():
     node = Node("Obj.")
-    assert node.element == "Obj."
+    assert node.data == "Obj."
     node2 = Node("Rhm.")
-    assert node2.element == "Rhm."
+    assert node2.data == "Rhm."
     node3 = Node(11114)
-    assert node3.element == 11114
+    assert node3.data == 11114
 
-def test_create_add_single_node_to_linked_list():
+def test_add_single_node_to_linked_list():
     linked_list = LinkedList()
     linked_list.add("Obj.")
-    assert linked_list._head.element == "Obj."
+    assert linked_list.head.data == "Obj."
+
+def test_add_two_nodes_to_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    assert linked_list.head.data == "Obj."
+    linked_list.add("Rhm.")
+    assert linked_list.head.next.data == "Rhm."
+
+def test_add_three_nodes_to_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    assert linked_list.head.data == "Obj."
+    linked_list.add("Rhm.")
+    assert linked_list.head.next.data == "Rhm."
+    linked_list.add("Jpz.")
+    assert linked_list.head.next.data == "Rhm."
+    assert linked_list.head.next.next.data == "Jpz."
