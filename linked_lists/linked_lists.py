1d523c7ba9888194acb54639fbcec9abd497f0f2
LazySusanoo <liam.drans@gmail.com>
2024-09-17 11:40:30 +0100

    adds delete node function to linked list class and testing suite

--- linked_lists/linked_lists.py
+++ linked_lists/linked_lists.py
@@ -12,27 +12,45 @@ class LinkedList:
         if self.length == 0:
             self.head = Node(data)
             self.tail = self.head
-            self.length += 1
         
         elif self.length == 1:
             self.head.next = Node(data)
             self.tail = self.head.next
-            self.length += 1
         
         else:
             self.tail.next = Node(data)
             self.tail = self.tail.next
-            self.length += 1
+        self.length += 1
+
+    def delete(self, data):
+        if isinstance(data, Node) and data == self.head or data == self.head.data:
+                self.head = self.head.next
+                self.length -= 1
+                print(f"Head node '{[data.data if isinstance(data, Node) else data][0]}' deleted")
+                return True
+        if self.head.next:
+            curr_node = self.head.next
+            prev_node = self.head
+            while curr_node:
+                if isinstance(data, Node) and data == curr_node or data == curr_node.data:
+                    if curr_node.next:
+                        prev_node.next = curr_node.next
+                        curr_node.next = None
+                        print(f"Inner node '{[data.data if isinstance(data, Node) else data][0]}' deleted")
+                        self.length -= 1
+                    else:
+                        prev_node.next = None
+                        print(f"Tail node '{[data.data if isinstance(data, Node) else data][0]}' deleted")
+                        self.length -= 1
+                prev_node = curr_node
+                curr_node = curr_node.next
 
     def listing(self):
         return_list = []
-        for i in range(self.length):
-            if i == 0:
-                return_list.append(self.head.data)
-            else:
-                next_str = "next."
-                eval_str = f"self.head.{next_str*i}data"
-                return_list.append(eval(eval_str)) # works but need to refactor to a iterator
+        curr_node = self.head
+        while curr_node:
+            return_list.append(curr_node.data)
+            curr_node = curr_node.next
         return return_list
 
 
--- test/test_linked_lists.py
+++ test/test_linked_lists.py
@@ -34,4 +34,73 @@ def test_add_multiple_nodes_to_linked_list():
     assert third_node.data == "Jpz."
     assert fourth_node.data == "Wz."
     assert len(linked_list) == 4
+
+def test_linked_list_listing():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    linked_list.add("Rhm.")
+    linked_list.add("Jpz.")
+    linked_list.add("Wz.")
     assert linked_list.listing() == ['Obj.', 'Rhm.', 'Jpz.', 'Wz.']
+
+def test_delete_head_note_of_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    first_node = linked_list.head
+    linked_list.add("Rhm.")
+    second_node = first_node.next
+    linked_list.add("Jpz.")
+    third_node = second_node.next
+    linked_list.delete(first_node)
+    assert linked_list.head == second_node
+    assert linked_list.head.next == third_node
+
+def test_delete_inner_nodes_of_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    first_node = linked_list.head
+    linked_list.add("Rhm.")
+    second_node = first_node.next
+    linked_list.add("Jpz.")
+    third_node = second_node.next
+    linked_list.add("Wz.")
+    fourth_node = third_node.next
+    linked_list.delete(second_node)
+    assert third_node == first_node.next
+    assert second_node.next is None
+    assert linked_list.listing() == ['Obj.', 'Jpz.', 'Wz.']
+    assert len(linked_list) == 3
+    linked_list.delete(third_node)
+    assert fourth_node == first_node.next
+    assert third_node.next is None
+    assert linked_list.listing() == ['Obj.', 'Wz.']
+    assert len(linked_list) == 2
+
+def test_delete_tail_nodes_of_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    first_node = linked_list.head
+    linked_list.add("Rhm.")
+    second_node = first_node.next
+    linked_list.add("Jpz.")
+    third_node = second_node.next
+    linked_list.add("Wz.")
+    fourth_node = third_node.next
+    linked_list.delete(fourth_node)
+    assert linked_list.listing() == ['Obj.', 'Rhm.', 'Jpz.']
+
+def test_delete_node_by_string_method_of_linked_list():
+    linked_list = LinkedList()
+    linked_list.add("Obj.")
+    first_node = linked_list.head
+    linked_list.add("Rhm.")
+    second_node = first_node.next
+    linked_list.add("Jpz.")
+    third_node = second_node.next
+    linked_list.add("Wz.")
+    fourth_node = third_node.next
+    linked_list.delete("Rhm.")
+    assert linked_list.listing() == ['Obj.', 'Jpz.', 'Wz.']
+    linked_list.delete("Wz.")
+    assert linked_list.listing() == ['Obj.', 'Jpz.']
+    assert len(linked_list) == 2
