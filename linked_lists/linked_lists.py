11728b01e6b6765c3714eb2bde15eb4969ba7984
LazySusanoo <liam.drans@gmail.com>
2024-09-17 10:37:12 +0100

    add listing functionality and testing

--- linked_lists/linked_lists.py
+++ linked_lists/linked_lists.py
@@ -2,7 +2,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.length = 0 
+        self.length = 0
 
     def __len__(self):
         return self.length
@@ -22,8 +22,20 @@ class LinkedList:
         else:
             self.tail.next = Node(data)
             self.tail = self.tail.next
-        print(data)
-        
+            self.length += 1
+
+    def listing(self):
+        return_list = []
+        for i in range(self.length):
+            if i == 0:
+                return_list.append(self.head.data)
+            else:
+                next_str = "next."
+                eval_str = f"self.head.{next_str*i}data"
+                return_list.append(eval(eval_str)) # works but need to refactor to a iterator
+        return return_list
+
+
 class Node:
     def __init__(self, data=None):
         self.data = data
--- test/test_linked_lists.py
+++ test/test_linked_lists.py
@@ -20,12 +20,18 @@ def test_add_two_nodes_to_linked_list():
     linked_list.add("Rhm.")
     assert linked_list.head.next.data == "Rhm."
 
-def test_add_three_nodes_to_linked_list():
+def test_add_multiple_nodes_to_linked_list():
     linked_list = LinkedList()
     linked_list.add("Obj.")
-    assert linked_list.head.data == "Obj."
+    first_node = linked_list.head
     linked_list.add("Rhm.")
-    assert linked_list.head.next.data == "Rhm."
+    second_node = first_node.next
     linked_list.add("Jpz.")
-    assert linked_list.head.next.data == "Rhm."
-    assert linked_list.head.next.next.data == "Jpz."
+    third_node = second_node.next
+    linked_list.add("Wz.")
+    fourth_node = third_node.next
+    assert second_node.data == "Rhm."
+    assert third_node.data == "Jpz."
+    assert fourth_node.data == "Wz."
+    assert len(linked_list) == 4
+    assert linked_list.listing() == ['Obj.', 'Rhm.', 'Jpz.', 'Wz.']
