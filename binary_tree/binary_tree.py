fd0d824c1e46e66a6b87f4f946b6a2c22e524985
LazySusanoo <liam.drans@gmail.com>
2024-09-20 15:02:04 +0100

    adds delete function to BinaryTree and testing suite - ensuring Tree is always complete

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -7,6 +7,29 @@ class BinaryTree:
             self.len = 1
             self.root = Node(data)
 
+    def _return_parent_by_data(self, data):
+
+        node = self.root
+
+        for i in reversed(range(self.len+1)[1:]):
+            for bit in bin(i)[3:-1]:
+                node = [node.left, node.right][int(bit)]
+            print(node.data)
+            if i & 1:
+                if node.right.data == data:
+                    return (node, int(bin(i)[-1]))
+            else:
+                if node.left.data == data:
+                    return (node, int(bin(i)[-1]))
+            node = self.root
+
+    def _return_parent_by_position(self, position):
+
+        node = self.root
+        for bit in bin(position)[3:-1]:
+            node = [node.left, node.right][int(bit)]
+        return (node, int(bin(position)[-1]))
+
     def append(self, data):
 
         self.len += 1
@@ -32,14 +55,58 @@ class BinaryTree:
             for bit in bin(i)[3:]:
                 node = [node.left, node.right][int(bit)]
             if node.data == data:
-                print(f"{data} found at position {i} of {self.name}.")
                 return (node.data, i)
             else:
                 node = self.root
         print(f"{data} not found in {self.name}.")
         return False
+    
+    def delete(self, data):
 
+        del_position = self.find(data)[1]
+        print(del_position)
 
+        parent_node = self._return_parent_by_data(data)
+        final_parent_node = self._return_parent_by_position(self.len)
+        replacing_node = [final_parent_node[0].right if final_parent_node[1] & 1 else final_parent_node[0].left][0]
+
+        if self.root.data == data:
+            right = self.root.right
+            left = self.root.left
+            self.root = replacing_node
+            self.root.right = right
+            self.root.left = left
+        else:
+            if parent_node[1] & 1:
+                if final_parent_node[1] & 1:
+                    replacing_node.right = parent_node[0].right.right
+                    replacing_node.left = parent_node[0].right.left
+                    parent_node[0].right = replacing_node
+                    final_parent_node[0].right = None
+                else:
+                    replacing_node.right = parent_node[0].right.right
+                    replacing_node.left = parent_node[0].right.left
+                    parent_node[0].right = replacing_node
+                    final_parent_node[0].left = None
+            else:
+                if final_parent_node[1] & 1:
+                    replacing_node.right = parent_node[0].left.right
+                    replacing_node.left = parent_node[0].left.left
+                    parent_node[0].left = replacing_node
+                    final_parent_node[0].right = None
+                else:
+                    replacing_node.right = parent_node[0].left.right
+                    replacing_node.left = parent_node[0].left.left
+                    parent_node[0].left = replacing_node
+                    final_parent_node[0].left = None
+
+        self.len -= 1
+
+        if del_position == self.len+1:
+            print(f"{data} successfully deleted from the end of {self.name}.")
+        else:
+            print(f"{data} successfully deleted, {replacing_node.data} replaced at {self.name} position {del_position}.")
+        
 
 class Node:
     def __init__(self, data):
--- test/test_binary_tree.py
+++ test/test_binary_tree.py
@@ -1,5 +1,5 @@
 import pytest
-from binary_tree.binary_tree import BinaryTree, Node
+from src.binary_tree import BinaryTree, Node
 
 @pytest.fixture
 def binary_tree():
@@ -49,3 +49,45 @@ def test_appends_to_multiple_levels_of_the_binary_tree(binary_tree):
     assert binary_tree.root.right.right.data == "Rhein."
     assert binary_tree.root.left.left.left.data == "Lullaby."
     assert binary_tree.len == 8
+
+def test_find_on_binary_tree(binary_tree):
+
+    assert binary_tree.find("Lerix.") == ("Lerix.", 5)
+    assert binary_tree.find("Lullaby.") == ("Lullaby.", 8)
+    assert binary_tree.find("Obj.") == ("Obj.", 1)
+    binary_tree.append("Luluru.")
+    assert binary_tree.find("Luluru.") == ("Luluru.", 9)
+
+
+def test_adding_100_to_binary_tree_and_using_find():
+    test_tree = BinaryTree()
+    for i in range(100)[1:]:
+        test_tree.append(i)
+    test_tree.append("Obj.")
+    assert test_tree.find(64) == (64, 64)
+    assert test_tree.find("Obj.") == ("Obj.", 100)
+
+def test_delete_root_node(binary_tree):
+    binary_tree.delete("Obj.")
+    assert binary_tree.find("Obj.") is False
+    assert binary_tree.root.data == "Lullaby."
+    assert binary_tree.len == 7
+    assert binary_tree.root.right.data == "Rhm."
+    assert binary_tree.root.left.data == "Leffe."
+
+def test_delete_position_5_node(binary_tree):
+    binary_tree.delete("Lerix.")
+    assert binary_tree.find("Lerix.") is False
+    assert binary_tree.find("Lullaby.") == ("Lullaby.", 5)
+    assert binary_tree.len == 7
+
+def test_delete_end_node(binary_tree):
+    binary_tree.delete("Lullaby.")
+    assert binary_tree.find("Lullaby.") is False
+    assert binary_tree.len == 7
+
+def test_delete_position_3_node(binary_tree):
+    binary_tree.delete("Rhm.")
+    assert binary_tree.find("Lullaby.") == ("Lullaby.", 3)
+    assert binary_tree.find("Relic.") == ("Relic.", 6)
+    assert binary_tree.find("Rhein.") == ("Rhein.", 7)
