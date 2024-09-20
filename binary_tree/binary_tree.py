74a8be49809dd2efafecd9927bbc8dd23ce8cf89
LazySusanoo <liam.drans@gmail.com>
2024-09-20 14:23:04 +0100

    adds find function to binarytree, returning tuple containing position and testing

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,25 +1,44 @@
 class BinaryTree:
-    def __init__(self, data=None):
+    def __init__(self, data=None, name="BinaryTree"):
         self.root = None
+        self.len = 0
+        self.name = name
         if data:
+            self.len = 1
             self.root = Node(data)
 
     def append(self, data):
 
-            self.len += 1
+        self.len += 1
 
-            if not self.root:
-                self.root = Node(data)
-                return
+        if not self.root:
+            self.root = Node(data)
+            return
 
-            node = self.root
+        node = self.root
 
-            for bit in bin(self.len)[3:-1]:
+        for bit in bin(self.len)[3:-1]:
+            node = [node.left, node.right][int(bit)]
+        if self.len & 1:
+            node.right = Node(data)
+        else:
+            node.left = Node(data)
+
+    def find(self, data):
+
+        node = self.root
+
+        for i in reversed(range(self.len+1)[1:]):
+            for bit in bin(i)[3:]:
                 node = [node.left, node.right][int(bit)]
-            if self.len & 1:
-                node.right = Node(data)
+            if node.data == data:
+                print(f"{data} found at position {i} of {self.name}.")
+                return (node.data, i)
             else:
-                node.left = Node(data)
+                node = self.root
+        print(f"{data} not found in {self.name}.")
+        return False
+
 
 
 class Node:
